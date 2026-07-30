207bb4679b0c578ff35438bd8db64d74647915d1
Fm498 <fmsign7@gmail.com>
2026-07-30 16:26:59 +0100

    Delete start.sh

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Simple launcher
-set -e
-cd "$(dirname "$0")/backend"
-export PORT="${PORT:-8080}"
-export HOST="${HOST:-0.0.0.0}"
-echo "Starting LASTMA Portal on port $PORT …"
-exec python3 main.py
